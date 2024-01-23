7371b711f8791b08bdb77331b5f3de265af7911f
Paul Elisha <ireajao@yahoo.com>
2024-01-23 01:31:04 +0100

    Updated file with extra data

--- ethscript.js
+++ ethscript.js
@@ -2,7 +2,7 @@
 const { ethers } = require("ethers");
 
 const INFURA_ID = '4d4628a86a0246c39f4d0151765ac867'
-const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
+const provider = new ethers.getDefaultProvider('mainnet');
 
 const address = '0xdE449A556Db775CCBB09477ba81F95FFA4683759'
 
@@ -10,9 +10,9 @@ const main = async () => {
     const balance = await provider.getBalance(address);
     const nonce = await provider.getTransactionCount(address);
     const blockNumber = await provider.getBlockNumber(address);
-    console.log(ethers.utils.formatEther(balance));
-    console.log(ethers.utils.formatEther(nonce));
-    
+    console.log(ethers.formatEther(balance));
+    console.log(ethers.formatEther(nonce));
+
 }
 
 main();
--- script.js
+++ script.js
@@ -2,15 +2,15 @@
 const { ethers } = require("ethers");
 
 const INFURA_ID = '4d4628a86a0246c39f4d0151765ac867'
-const provider = new ethers.providers.JsonRpcProvider(`https://sepolia.infura.io/v3/${INFURA_ID}`)
+const provider = new ethers.getDefaultProvider('sepolia');
 
-const address = '0x61DE0932AD49E9cd964d278b1D7790C42533B89F'
+const address = '0x9D3de545F58C696946b4Cf2c884fcF4f7914cB53'
 
 const main = async () => {
     const balance = await provider.getBalance(address);
     const nonce = await provider.getTransactionCount(address);
     const blockNumber = await provider.getBlockNumber(address);
-    console.log(ethers.utils.formatEther(balance));
+    console.log(ethers.formatEther(balance));
     console.log(nonce);
     console.log(blockNumber);
 }
